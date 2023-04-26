c31cde4a25fc02045cc9f377e34540b7b5f31bd3
shaman <shishengxiang@auroramob.com>
2023-04-26 23:53:39 +0800

    chrome

--- Test.py
+++ Test.py
@@ -1,16 +1,25 @@
-def main():
-    num = int(input('Number of rows: '))
-    yh = [[]] * num
-    for row in range(len(yh)):
-        yh[row] = [None] * (row + 1)
-        for col in range(len(yh[row])):
-            if col == 0 or col == row:
-                yh[row][col] = 1
-            else:
-                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
-            print(yh[row][col], end='\t')
-        print()
+import time
 
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-if __name__ == '__main__':
-    main()
+options = Options()
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver.maximize_window()
+# driver.get("https://mail.yandex.com/")
+driver.get("https://mail360.yandex.com/premium-plans?from=mail_landing")
+driver.implicitly_wait(10)
+try:
+    button = driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div/div[3]/section[1]/div/a[2]')
+    print(f'button:{button}')
+    button.click()
+except NoSuchElementException as e:
+    print("没找到按钮")
+    driver.quit();
+
+time.sleep(5)
